d1baaf2c782e009c9452dfe6b4d57bced5bb21ca
Mansi Tandel <97576290+MansiTandel@users.noreply.github.com>
2023-06-06 12:42:05 -0700

    Update server.js
    added the path const.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 const port = process.env.PORT || 5000;
+const path = require('path');
 app.use(express.json())
 
 const usersRoute= require('./routes/usersRoute');
